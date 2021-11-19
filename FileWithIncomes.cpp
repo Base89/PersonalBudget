a08a6e21edc2f52d5cce9fbf7f20fe7fc295f216
Sebastian Mynarski <smynarski89@gmail.com>
2021-11-19 20:54:17 +0100

    Added new features in FileWithIncomes & one beta feature loadIncomes as feat to test

---
+++ FileWithIncomes.cpp
@@ -0,0 +1,106 @@
+#include "FileWithIncomes.h"
+
+Income FileWithIncomes::getIncomeData(CMarkup xml)
+{
+    Income income;
+
+    xml.IntoElem();
+
+    xml.FindElem("IncomeId");
+    MCD_STR strIncomeId = xml.GetData();
+    income.setIncomeId(AuxiliaryMethods::convertStringToInt(strIncomeId));
+
+    xml.FindElem("UserId");
+    MCD_STR strUserId = xml.GetData();
+    income.setUserId(AuxiliaryMethods::convertStringToInt(strUserId));
+
+    xml.FindElem("Date");
+    MCD_STR strDate = xml.GetData();
+    income.setDate(AuxiliaryMethods::convertStringToInt(strDate));
+
+    xml.FindElem("Item");
+    MCD_STR strItem = xml.GetData();
+    income.setItem(strItem);
+
+    xml.FindElem("Amount");
+    MCD_STR strAmount = xml.GetData();
+    income.setAmount(AuxiliaryMethods::convertStringToDouble(strAmount));
+
+    xml.OutOfElem();
+
+    return income;
+}
+
+int FileWithIncomes::getLastIncomeIdFromFile(CMarkup xml)
+{
+    xml.FindElem();
+    xml.IntoElem();
+
+    while (xml.FindElem("Income")) {}
+
+    xml.IntoElem();
+
+    xml.FindElem("IncomeId");
+    MCD_STR strIncomeId = xml.GetData();
+
+    return AuxiliaryMethods::convertStringToInt(strIncomeId);
+}
+
+vector <Date> FileWithIncomes::getDatesToPeriod(string startingDate, string closingDate)
+{
+
+}
+
+vector <Income> FileWithIncomes::searchIncomesBySelectedPeriod(vector <Date>)
+{
+
+}
+
+void FileWithIncomes::addIncomeToFile(Income income)
+{
+    CMarkup xml;
+
+    xml = loadFile(getFileName());
+
+    xml.FindElem();
+    xml.IntoElem();
+    xml.AddElem("Income");
+    xml.IntoElem();
+    xml.AddElem("IncomeId", income.getIncomeId());
+    xml.AddElem("UserId", income.getUserId());
+    xml.AddElem("Date", income.getDate());
+    xml.AddElem("Item", income.getItem());
+    xml.AddElem("Amount", income.getAmount());
+
+    xml.OutOfElem();
+    xml.Save(getFileName() + ".xml");
+}
+
+vector <Income> FileWithIncomes::loadIncomesOfLoggedInUserFromFile(int loggedInUserId)
+{
+    CMarkup xml;
+    vector <Income> incomes;
+    string strUserId = AuxiliaryMethods::convertIntToString(loggedInUserId);
+
+    xml = loadFile(getFileName());
+    xml.FindElem();
+    xml.IntoElem();
+
+    while (xml.FindElem("Income"))
+    {
+        xml.FindChildElem("UserId");
+        if (xml.GetChildData() == strUserId)
+        {
+            incomes.push_back(getIncomeData(xml));
+        }
+    }
+    cout << "Loaded incomes from file. " << endl;
+
+    return incomes;
+}
+
+int FileWithIncomes::getLastIncomeId()
+{
+    return lastIncomeId;
+}
+
--- FileWithIncomes.h
+++ FileWithIncomes.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "Income.h"
+#include "Date.h"
 #include "TextFile.h"
 #include "AuxiliaryMethods.h"
 
@@ -12,7 +13,22 @@ using namespace std;
 
 class FileWithIncomes : public TextFile
 {
+    int lastIncomeId;
 
+    Income getIncomeData(CMarkup xml);
+    int getLastIncomeIdFromFile(CMarkup xml);
+    vector <Date> getDatesToPeriod(string startingDate, string closingDate);
+    vector <Income> searchIncomesBySelectedPeriod(vector <Date>);
+
+public:
+    FileWithIncomes(string fileName) : TextFile (fileName)
+    {
+        lastIncomeId = 0;
+    }
+
+    void addIncomeToFile(Income income);
+    vector <Income> loadIncomesOfLoggedInUserFromFile(int loggedInUserId);
+    int getLastIncomeId();
 };
 
 #endif
